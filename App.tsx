53139e918dd076f26c13085f691947611d9ec827
Matheus Alves <matheusfrancisco0330@gmail.com>
2023-02-02 12:09:27 -0300

    setup: iniciando o projeto

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.title}>Hello world!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#1e1e1e',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title:{
+    color: '#f6f6f6'
+  }
 });
